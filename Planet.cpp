6e8d8a360cd8d85d6942adf46df3823040eec606
Piotrixek <113201557+Piotrixek@users.noreply.github.com>
2025-03-26 22:16:06 +0100

    .
    .

--- Planet.cpp
+++ Planet.cpp
@@ -1,7 +1,9 @@
 // Planet.cpp
 #include "Planet.h"
+#include <glm/gtc/matrix_transform.hpp>
 Planet::Planet(const glm::vec3& pos, const glm::vec3& vel, float mass, float radius)
-    : position(pos), velocity(vel), mass(mass), radius(radius), acceleration(glm::vec3(0.0f)) {}
+    : position(pos), velocity(vel), mass(mass), radius(radius), acceleration(glm::vec3(0.0f)),
+    rotationAxis(glm::vec3(0.0f, 1.0f, 0.0f)), rotationAngle(0.0f), angularVelocity(0.0f), color(glm::vec3(1.0f)) {}
 void Planet::applyForce(const glm::vec3& force) {
     acceleration += force / mass;
 }
@@ -9,7 +11,29 @@ void Planet::update(float dt) {
     velocity += acceleration * dt;
     position += velocity * dt;
     acceleration = glm::vec3(0.0f);
+    updateRotation(dt);
 }
-const glm::vec3& Planet::getPosition() const { return position; }
-float Planet::getMass() const { return mass; }
-float Planet::getRadius() const { return radius; }
+void Planet::updateRotation(float dt) {
+    rotationAngle += angularVelocity * dt;
+}
+const glm::vec3& Planet::getPosition() const {
+    return position;
+}
+float Planet::getMass() const {
+    return mass;
+}
+float Planet::getRadius() const {
+    return radius;
+}
+glm::mat4 Planet::getModelMatrix() const {
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
+    model = glm::rotate(model, rotationAngle, rotationAxis);
+    model = glm::scale(model, glm::vec3(radius));
+    return model;
+}
+void Planet::setColor(const glm::vec3& col) {
+    color = col;
+}
+const glm::vec3& Planet::getColor() const {
+    return color;
+}
--- Planet.h
+++ Planet.h
@@ -6,13 +6,21 @@ public:
     Planet(const glm::vec3& pos, const glm::vec3& vel, float mass, float radius);
     void applyForce(const glm::vec3& force);
     void update(float dt);
+    void updateRotation(float dt);
     const glm::vec3& getPosition() const;
     float getMass() const;
     float getRadius() const;
+    glm::mat4 getModelMatrix() const;
+    void setColor(const glm::vec3& col);
+    const glm::vec3& getColor() const;
     glm::vec3 position;
     glm::vec3 velocity;
     float mass;
     float radius;
+    glm::vec3 rotationAxis;
+    float rotationAngle;
+    float angularVelocity;
 private:
     glm::vec3 acceleration;
+    glm::vec3 color;
 };
